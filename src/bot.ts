73a11d88beaec842f16e27c77e722c1e1500225b
João Vanzuita <joao.vanzuita@de.bosch.com>
2020-03-23 22:37:34 +0100

    add more questions, add timer

--- src/bot.ts
+++ src/bot.ts
@@ -2,15 +2,17 @@ import { Client, Message } from 'discord.js';
 import { inject, injectable } from 'inversify';
 import TYPES from './types';
 import MessageResponder from './services/message-responder';
-import { showRandomQuestion, checkAnswer } from './engine/index';
+import { showRandomQuestion, checkAnswer } from './engine';
 
 @injectable()
 class Bot {
+  private messageResponder: MessageResponder;
+
   private client: Client;
 
-  private readonly token: string;
+  private running = false;
 
-  private messageResponder: MessageResponder;
+  private readonly token: string;
 
   constructor(@inject(TYPES.Client) client: Client,
     @inject(TYPES.Token) token: string,
@@ -20,25 +22,44 @@ class Bot {
     this.messageResponder = messageResponder;
   }
 
+  public sleepAndNext(ms: number): Promise<string> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  public stop(): void {
+    this.running = false;
+  }
+
   public listen(): Promise<string> {
     this.client.once('ready', () => {
       console.log('Connected!');
     });
 
-    this.client.on('message', (message: Message) => {
-
+    this.client.on('message', async (message: Message) => {
       // todo: use code injection later
       // this.messageResponder.handle(message)
       //   .then(() => console.log('response sent'))
       //   .catch((error) => console.log('Error', error));
 
-      if (message.content.search('!q') >= 0) {
+      if (message.content.search('!start') >= 0 && !this.running) {
+        message.reply('');
+        message.react('🥰');
         message.reply(showRandomQuestion());
-        // message.react('🥰');
+        this.running = true;
       }
 
-      if (message.content.search('^[0-9]*$') >= 0) {
-        checkAnswer(message);
+      if (message.content.search('^[0-9]*$') >= 0 && !this.running) {
+        if (checkAnswer(message)) {
+          message.reply('Próxima pergunta em 10 segundos...');
+          await this.sleepAndNext(10000);
+          message.reply(showRandomQuestion());
+        }
+      }
+
+      // todo: only admin can stop
+      if (message.content.search('!stop') >= 0 && this.running) {
+        message.reply('Quiz encerrado!');
+        this.running = false;
       }
     });
 
--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,25 +1,30 @@
 import { Message } from 'discord.js';
 import * as schema from './schema.json';
 
-let quizActive = false;
 let randomQuestion;
 let question;
 
-
+/**
+ * Shuffle a question from Schema(JSON)
+ */
 const showRandomQuestion = (): string => {
   randomQuestion = Math.floor(Math.random() * Object.keys(schema).length) + 1;
   question = schema[randomQuestion].question;
   const answer1 = schema[randomQuestion].choices[1];
   const answer2 = schema[randomQuestion].choices[2];
-  // active quiz
-  quizActive = true;
   return `Pergunta: ${question}1. ${answer1}\n2. ${answer2}`;
 };
 
-const checkAnswer = (message: Message): void => {
-  if (quizActive && message.content === schema[randomQuestion].answer) {
+/**
+ * Assess if the answer is correct
+ * @param message
+ */
+const checkAnswer = (message: Message): boolean => {
+  if (message.content === schema[randomQuestion].answer) {
     message.reply('correto!');
+    return true;
   }
+  return false;
 };
 
 export { showRandomQuestion, checkAnswer };
